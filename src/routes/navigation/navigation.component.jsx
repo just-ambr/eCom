a0269d06e64f6adf48e6d5680fc7a8cf5e9ee7a2
just-ambr <104867096+just-ambr@users.noreply.github.com>
2023-11-29 00:30:20 +0100

    created shoppingcart icon and cart context, cart dropdown
    navigation component icon dropdown trigger l9

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,8 +1,12 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
-import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
 import { userSignOut } from "../../utils/firebase/firebase.utils";
 
@@ -10,6 +14,7 @@ import "./navigation.styles.scss";
 
 const Navigation = () => {
 	const { currentUser } = useContext(UserContext);
+	const { isCartOpen } = useContext(CartContext);
 
 	return (
 		<Fragment>
@@ -17,7 +22,6 @@ const Navigation = () => {
 				<Link className="logo-container" to="/">
 					<CrwnLogo className="Logo" />
 				</Link>
-
 				<div className="nav-links-container">
 					<Link className="nav-link" to="/shop">
 						SHOP
@@ -32,7 +36,10 @@ const Navigation = () => {
 							SIGN IN
 						</Link>
 					)}
+					<CartIcon />
 				</div>
+				{isCartOpen && <CartDropdown />}{" "}
+				{/*if both true, return the last thing u gave(dropdown)  */}
 			</div>
 			<Outlet />
 		</Fragment>
