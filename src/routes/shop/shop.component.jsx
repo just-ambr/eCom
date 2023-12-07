1be613f0187bba248ad987a319eb214b777ebc5f
just-ambr <104867096+just-ambr@users.noreply.github.com>
2023-12-07 16:28:00 +0100

    add collection and document, nested routes, category page
    adding collection and documents to DB, nested routes, category page, categoriesMap, get products from firestore l10

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,22 +1,15 @@
-import { useContext } from "react";
-
-import { ProductsContext } from "../../contexts/products.context";
-import ProductCard from "../../components/product-card/product-card.component";
+import { Route, Routes } from "react-router-dom";
 
 import "./shop.style.scss";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
 
 const Shop = () => {
-	const { products } = useContext(ProductsContext);
-
 	return (
-		<div className="product-card-container">
-			{products.map((product) => (
-				/*<div key={id}>
-					<h1>{name}</h1>
-				</div>*/
-				<ProductCard key={product.id} product={product} />
-			))}
-		</div>
+		<Routes>
+			<Route index element={<CategoriesPreview />} />
+			<Route path=":category" element={<Category />} />
+		</Routes>
 	);
 };
 
